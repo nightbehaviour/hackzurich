27676f247b85e9ae47e245ba410df5cfa467a492
Rebecca Kuhn <rebecca@furbo.ch>
2019-09-29 01:20:47 +0200

    ff

--- application.py
+++ application.py
@@ -7,16 +7,22 @@ from random import *
 application = app = Flask(__name__,
         static_folder="./dist/static",
         template_folder="./dist")
-CORS(application)
+CORS(app)
 
 # Api endpoint
 @app.route('/api/get-offers', methods=['GET', 'POST'])
 
 def get_the_offers():
 
-    travel_date = '2019-10-27'
-    travel_time = '10:00'
-    travel_orig_name = 'Zurich'
+    if request.method == "POST":
+        travel_date = '2019-10-27'
+        travel_time = '10:00'
+        travel_orig_name = 'Zurich'
+
+    else:
+        travel_date = '2019-10-27'
+        travel_time = '10:00'
+        travel_orig_name = 'Zurich'
 
     s_request = sbbHackClass.SurpriseRequest()
     response = s_request.go_for_offers(travel_date,
